5d138713d289df687a85c307fb70b75a2887e197
Doug Walter <doug@kosmojo.com>
2015-08-26 16:22:46 +0200

    all events endpoint working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api/events', events)
+// app.use('/api/events', events);
+app.get('/api/events', events);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
